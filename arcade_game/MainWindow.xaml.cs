dec428b5879cd97583c35523fff9450f41f379d2
mrgiel <47106654+mrgiel@users.noreply.github.com>
2021-10-06 11:09:14 +0200

    naam invullen werkt, nog geen koppeling met db.

--- arcade_game/MainWindow.xaml.cs
+++ arcade_game/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,24 @@ namespace arcade_game
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string teamname;
+        public string player1;
+        public string player2;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            teamname = teamnaam.Text;
+            player1 = speler1.Text;
+            player2 = speler2.Text;
+            System.Diagnostics.Debug.WriteLine("teamnaam is: " + teamname);
+            System.Diagnostics.Debug.WriteLine("speler 1 is: " + player1);
+            System.Diagnostics.Debug.WriteLine("speler 2 is: " + player2);
+        }
+
+
     }
 }
