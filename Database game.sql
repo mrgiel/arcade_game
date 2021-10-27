dbca56c03392ed6d480c44584fe0045b32b88d54
youssef <youssef.charef-khodja@student.nhlstenden.com>
2021-10-27 13:53:12 +0200

    database

---
+++ Database game.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Game] (
+    [Id]        INT        IDENTITY (1, 1) NOT NULL,
+    [Teamnaam]  NCHAR (16) NOT NULL,
+    [Speler1]   NCHAR (16) NOT NULL,
+    [Speler2]   NCHAR (16) NOT NULL,
+    [Highscore] INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
